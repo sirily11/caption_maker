ba272798772f0e1d860d76f2f3db9b3c5e512cf7
sirily11 <sirily1997@gmail.com>
2020-02-12 20:07:16 -0600

    added video package

---
+++ packages/video_player/video_player/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/liqiwei/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/2tb SSD/projects/captions_maker/packages/video_player/video_player/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/liqiwei/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
